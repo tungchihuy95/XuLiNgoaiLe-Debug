bed762a58c0e7358cca9120875e2854d0b6f2a5e
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-24 14:45:57 +0700

    add

---
+++ src/ArrayIndexOutOfBoundsException/ArrayExample.java
@@ -0,0 +1,31 @@
+package ArrayIndexOutOfBoundsException;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sach phan tu cua mang: ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.println(arr[i] + " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui long nhap chi so cua mot phan tu bat ky: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Gia tri cua phan tu so chi so 5" + x + "la" + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chi so vuot qua gioi han cua mang");
+        }
+    }
+}
